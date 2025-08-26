313d1e603e814ee870de14d48d72a7e3c1f205aa
Chris Ferrantelli <chrisdraper033@gmail.com>
2025-08-26 15:08:58 -0400

    added commands to cogs, and added help command

---
+++ cogs/play_audio.py
@@ -0,0 +1,28 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+class Fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+load_dotenv()
+AUDIO_PATH = os.getenv('AUDIO_PATH')
+FFMPEG_PATH = os.getenv('FFMPEG_PATH')
+
+
+@commands.command()
+async def play_cope(ctx):
+   if not ctx.message.author.voice:
+      await ctx.send("You are not in a voice channel")
+      return
+   
+   vc = ctx.message.author.voice.channel
+   voiceCha = await vc.connect()
+   await ctx.send(f"Joined {vc.name}")
+   voiceCha.play(discord.FFmpegPCMAudio(source=AUDIO_PATH, executable=FFMPEG_PATH))
+
+
+async def setup(bot):
+    await bot.add_cog(Fun(bot))
