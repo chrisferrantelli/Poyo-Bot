cebaa1de2d3258ad3c4b4c93b88cc9b272e14f14
Chris Ferrantelli <chrisdraper033@gmail.com>
2025-08-26 15:34:20 -0400

    more cog updates for troubleshooting

--- cogs/ego.py
+++ cogs/ego.py
@@ -3,17 +3,17 @@ import random
 
 from discord.ext import commands
 
-class EgoCog(commands.Cog):
+class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
 
-@commands.command()
-async def ego(ctx, user: discord.Member= None):
-    if user == ctx.author or user == None:
-        await ctx.send(f'Your ego is {random.randint(1,100)}%')
-    else:
-        await ctx.send(f'{user.mention}`s ego is {random.randint(1,100)}%')
+    @commands.command()
+    async def ego(ctx, user: discord.Member= None):
+        if user == ctx.author or user == None:
+            await ctx.send(f'Your ego is {random.randint(1,100)}%')
+        else:
+            await ctx.send(f'{user.mention}`s ego is {random.randint(1,100)}%')
 
 async def setup(bot):
-    await bot.add_cog(EgoCog(bot))
+    await bot.add_cog(Fun(bot))
