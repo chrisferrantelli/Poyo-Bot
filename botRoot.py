3f38a114e0939636adc814ead0b6ddd1aa2410b5
Chris Ferrantelli <chrisdraper033@gmail.com>
2025-08-26 14:13:47 -0400

    updated on_ready

--- botRoot.py
+++ botRoot.py
@@ -18,19 +18,18 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 
-@bot.event
-async def on_ready():
-   for filename in os.listdir("./cogs"):
-      if filename.endswith(".py"):
-         bot.load_extension(f"cogs.{filename[:-3]}")
-         print(f"The {filename[:-3]} cog has loaded")
-      else:
-         print(f"Unable to load {filename[:-3]}")
-
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+       for filename in os.listdir("./cogs"):
+         if filename.endswith(".py"):
+            bot.load_extension(f"cogs.{filename[:-3]}")
+            print(f"The {filename[:-3]} cog has loaded")
+
+         else:
+            print(f"Unable to load {filename[:-3]}")
+
+print(f'{bot.user} has connected to Discord!')
 
 
 @bot.command()
