b7cea43c74d02d482a627bea7ce69a703cad341e
Chris Ferrantelli <chrisdraper033@gmail.com>
2025-08-26 18:33:30 -0400

    updated poyoball

--- cogs/poyoball.py
+++ cogs/poyoball.py
@@ -1,5 +1,8 @@
 import discord
 import random
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+
 
 from discord.ext import commands
 
@@ -7,6 +10,7 @@ class PoyoballCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    
     @commands.command()
     async def poyoball(self, ctx, *, question: str = None):
 
@@ -41,8 +45,23 @@ class PoyoballCog(commands.Cog):
                 "poyo... (shakes head)",
                 "poynope."
         ]
+        resp = random.choice(responses)
+    
+        # retreives the kirby board template
+        with Image.open("resources/kirby_board.jpg") as img:
+            draw = ImageDraw.Draw(img)
+            
+            font = ImageFont.truetype("arial.ttf", 20)
+            
+            # draws the text on the image
+            draw.text((90, 25), resp, font=font, fille=(0,0,0))
+            
+            # saves to memorys
+            buf = BytesIO()
+            img.save(buf, format="PNG")
+            buf.seek(0)
+            
+        await ctx.send(file=discord.File(buf, filename="poyoball.png"))
 
-        await ctx.send(f'{random.choice(responses)}')
-
-async def setup(bot):
-    await bot.add_cog(PoyoballCog(bot))
+    async def setup(bot):
+        await bot.add_cog(PoyoballCog(bot))
